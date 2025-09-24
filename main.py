7088a41e8e25890ed6ceabfc3c54553c46cacb0a
Divijeshvarma.a <divijeshvarma.a@pidatacenters.com>
2025-09-24 17:05:17 +0530

    commit 1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 def index():
 
-    return "Congratulations, Divi"
+    return "Congratulations, Varma"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9090, debug=True)
